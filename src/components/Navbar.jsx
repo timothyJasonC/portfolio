3d0521fe4c23d3d0f6d0543a13d9de443cd98ee8
Timothy Jason <159093945+timothyJasonC@users.noreply.github.com>
2024-03-05 15:23:36 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar() {
         <header className="bg-gray-900 bg-opacity-90 w-screen h-[4rem] py-4 md:px-24 min-[240px]:px-1 text-white fixed">
             <div className="flex items-center gap-2 text-center justify-between">
                 <a href="#" className="text-2xl font-bold font-poppins " data-aos="fade-right" data-aos-duration="2000">Timothy</a>
-                <nav className=" flex gap-3 text-sm tracking-tighter ">
+                <nav className=" flex gap-3 text-sm tracking-tighter mx-4">
                     <a href="#about" className="font-poppins ease-linear duration-300 hover:text-teal-400" data-aos="fade-down" data-aos-duration="2000">About</a>
                     <a href="#projects" className="font-poppins ease-linear duration-300 hover:text-teal-400" data-aos="fade-down" data-aos-duration="2400">Projects</a>
                     <a href="#education" className="font-poppins ease-linear duration-300 hover:text-teal-400" data-aos="fade-down" data-aos-duration="2800">Education</a>
