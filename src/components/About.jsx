3eab629226caad6e1604ad1f3ec6258107b7f01e
Timothy Jason <timothycaviezel@gmail.com>
2024-04-21 09:51:47 +0700

    semi-final

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,16 +6,16 @@ export default function About() {
         Aos.init()
     })
     return (
-        <section id="about" className="text-white bg-gray-900 w-full h-screen flex flex-col-reverse md:flex-row justify-center items-center px-12 gap-6">
+        <section id="about" className="text-white pt-20 bg-gray-900 w-full h-screen flex flex-col-reverse md:flex-row justify-center items-center px-12 gap-6">
             <div className="flex items-center md:items-start min-[320px]:text-center flex-col gap-4">
-                <h1 data-aos="fade-right" data-aos-duration="1500" className="text-4xl font-bold ">I'm Timothy Jason</h1>
-                <div>
-                    <h2 data-aos="fade-right" data-aos-duration="1700" className="text-3xl text-teal-400 " >Fullstack Web Developer</h2>
+                <h1 data-aos="fade-right" data-aos-duration="1500" className="text-2xl md:text-4xl font-bold ">I'm Timothy Jason</h1>
+                <div className='text-animation'>
+                    <h2 data-aos="fade-right" data-aos-duration="1700" className="text-2xl md:text-4xl" >Fullstack Web Developer</h2>
                 </div>
-                <p data-aos="fade-right" data-aos-duration="1900" className="flex text-center md:text-start ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia unde iste voluptates. Asperiores laudantium exercitationem voluptatibus libero eligendi aut autem.</p>
+                <p data-aos="fade-right" data-aos-duration="1900" className="flex text-justify md:text-start text-sm md:text-xl">Welcome to my portfolio! As a Fullstack Web Developer, I specialize in creating captivating web experiences by blending compelling design with robust functionality. With expertise in frontend technologies like HTML, CSS, and JavaScript, as well as backend frameworks like Node.js and Express, I am equipped to handle all aspects of web development.
+                </p>
                 <div className="flex gap-2">
-                    <a data-aos="fade-up" data-aos-duration="2200" href="https://www.instagram.com/ezzel_09026/" className="border-solid w-32 text-center bg-teal-400 border-2 py-2 px-4 rounded-full text-xl font-semibold ease-linear duration-1000 hover:bg-teal-800 " >Hire Me</a>
-                    <a data-aos="fade-up" data-aos-duration="2400" href="https://www.instagram.com/ezzel_09026/" className="border-solid w-32 text-center bg-teal-400 border-2 py-2 px-4 rounded-full text-xl font-semibold ease-linear duration-300 hover:bg-teal-800 ">Let's Talk</a>
+                    <a data-aos="fade-up" data-aos-duration="2200" href="https://www.instagram.com/ezzel_09026/" className="border-solid w-32 text-center bg-teal-400 border-2 py-2 px-4 rounded-full text-xl font-semibold ease-linear duration-1000 hover:bg-teal-800 " >Let's Talk</a>
                 </div>
             </div>
             <img data-aos="fade-left" data-aos-duration="2000" className="w-[30vw] rounded-xl" src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1hbnxlbnwwfHwwfHx8MA%3D%3D" alt="" />
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,24 @@
 import '../App.css';
 import { useEffect } from 'react';
 import Aos from 'aos';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
+import nuartha from '../assets/nuartha.png';
 export default function Projects() {
-   useEffect(() =>{
-    Aos.init()
-   })
+    useEffect(() => {
+        Aos.init()
+    })
     return (
-        <section id="projects" className="text-white bg-gray-900 w-full h-full pb-[6rem] flex flex-col justify-center items-center px-12 gap-6 pt-32 overflow-hidden">
-            <h2 className="text-4xl font-semibold " data-aos="fade-down">My <span className="text-teal-400">Projects</span></h2>
+        <section id="projects" className="text-white bg-gray-900 w-full py-[6rem] flex flex-col justify-center items-center px-12 gap-6 overflow-hidden">
+            <h2 className="text-2xl md:text-4xl font-semibold " data-aos="fade-down">My <span className="text-teal-400">Projects</span></h2>
             <div className="flex flex-col gap-4 mt-4 ">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center ">
-                    <div className="py-4 w-auto h-auto px-16 w border-4 border-slate-400 rounded-lg bg-slate-300 bg-opacity-30 text-9xl text-center items-center justify-center" data-aos="zoom-in" data-aos-duration="1000">+</div>
-                    <p className="w-auto flex text-center md:w-auto" data-aos="zoom-in" data-aos-duration="1000">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque non placeat cupiditate facere! Quis, esse?</p>
+                    <img src={nuartha} className='w-full h-auto rounded-2xl' data-aos="zoom-in" data-aos-duration="1200" />
+                    <div className='flex flex-col' data-aos="zoom-in" data-aos-duration="1000">
+                        <p className="w-auto flex text-justify md:w-auto  text-sm md:text-base" >In this project, I utilized Tailwind CSS and Next.js as the main tech stack. The primary focus was on slicing UI for web interfaces. By using Tailwind CSS, I efficiently implemented the provided design into CSS code without the need for manual CSS writing. Next.js provided a robust framework for web application development, including the ability to create responsive and SEO-friendly web applications easily. Leveraging these technologies, I successfully delivered a web interface that aligns with the provided design, providing users with a consistent and engaging experience.</p>
+                        <a href="https://company-profile-ten-mu.vercel.app/" target="_blank" rel="noopener noreferrer" className="mt-4 w-40 bg-teal-500 hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition cursor-pointer duration-300 ease-in-out">
+                            Visit Website &raquo;
+                        </a>
+                    </div>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center ">
                     <div className="py-4 w-auto h-auto px-16 order-first md:order-last w border-4 border-slate-400 rounded-lg bg-slate-300 bg-opacity-30 text-9xl text-center items-center justify-center" data-aos="zoom-in" data-aos-duration="1200">+</div>
