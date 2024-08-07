85dd91f473a765bbf549491fb0676676b2076065
Timothy Jason <timothycaviezel@gmail.com>
2024-08-07 22:03:48 +0700

    remake version 3.0/

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ subsets: ["latin"], weight: ['400', '600'] });
 
 export const metadata: Metadata = {
   title: "Timothy's Portfolio",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={poppins.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,80 +1,57 @@
 import { projects } from '@/data'
+import { useScroll, motion, useTransform } from 'framer-motion'
 import React from 'react'
-import { PinContainer } from './ui/PinContainer'
-import { FaLocationArrow } from 'react-icons/fa'
 
 const RecentProjects = () => {
+    const scrollRef = React.useRef<HTMLDivElement>(null)
+
+    const { scrollYProgress } = useScroll({
+        target: scrollRef,
+        offset: ["start start", "center start"]
+    })
+
+    const xTranform = useTransform(scrollYProgress, [0, 0.5, 1], [0, -400, 0])
+
     return (
-        <div className='py-20' id='projects'>
+        <motion.div style={{
+            height: '40rem',
+            zIndex: 6,
+            position: 'relative',
+            translateX: xTranform
+        }} className='' id='projects'>
             <h1 className='heading text-black-100'>
                 A small selection of {' '}
                 <span className='text-purple'>Recent projects</span>
             </h1>
-            <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+            <div className="flex flex-wrap mb-40 items-center justify-center p-4 gap-16 mt-10">
                 {projects.map(({ id, title, des, img, iconLists, link }) => (
-                    <div
-                        className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-                        key={id}
-                    >
-                        <PinContainer
-                        href={link}
-                        title={link}>
-                            <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                                <div
-                                    className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                                    style={{ backgroundColor: "#13162D" }}
-                                >
-                                    <img src="/bg.png" alt="bgimg" />
-                                </div>
-                                <img
-                                    src={img}
-                                    alt="cover"
-                                    className="z-10 absolute bottom-0"
-                                />
+                    <div key={id} className="bg-white-100 relative rounded-lg hover:scale-105 shadow-md overflow-hidden transform transition duration-500 lg:min-h-[35rem] h-[28rem] sm:w-96 w-[80vw]">
+                        <img src={img} alt={title} className="h-64 object-cover" />
+
+                        <div className='flex flex-col justify-between'>
+
+                            <div className="p-4">
+                                <h3 className="text-xl font-semibold mb-2">{title}</h3>
+
+                                <p className="text-gray-600 mb-4">{des}</p>
                             </div>
 
-                            <h1 className="font-bold lg:text-xl md:text-md text-base line-clamp-1">
-                                {title}
-                            </h1>
-
-                            <p
-                                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                                style={{
-                                    color: "#BEC1DD",
-                                    margin: "1vh 0",
-                                }}
-                            >
-                                {des}
-                            </p>
-
-                            <div className="flex items-center justify-between mt-7 mb-3">
-                                <div className="flex items-center">
+                            <div className='absolute bottom-2 px-4'>
+                                <div className="flex space-x-2 mb-4">
                                     {iconLists.map((icon, index) => (
-                                        <div
-                                            key={index}
-                                            className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                                            style={{
-                                                transform: `translateX(-${5 * index + 2}px)`,
-                                            }}
-                                        >
-                                            <img src={icon} alt="icon5" className="p-2" />
-                                        </div>
+                                        <img key={index} src={icon} alt="Icon" className="w-6 h-6" />
                                     ))}
                                 </div>
 
-                                <div className="flex justify-center items-center">
-                                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                                        Check Live Site
-                                    </p>
-                                    <FaLocationArrow className="ms-3" color="#CBACF9" />
-                                </div>
+                                <a href={link} target="_blank" rel="noopener noreferrer" className="text-indigo-500 hover:text-indigo-700 cursor-pointer flex items-center">
+                                    Check Live Site <span className="ml-1">→</span>
+                                </a>
                             </div>
-                        </PinContainer>
+                        </div>
                     </div>
                 ))}
-
             </div>
-        </div>
+        </motion.div>
     )
 }
 
