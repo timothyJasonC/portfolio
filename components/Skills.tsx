a0eed86b71de8c6dc812b7be5b8ce5f6d3ee7b18
Timothy Jason <timothycaviezel@gmail.com>
2024-08-08 14:14:21 +0700

    fix mobile ui

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,9 +4,10 @@ import React from 'react'
 
 interface Props {
     children: React.ReactNode;
+    id: number
 }
 
-const TextWrapper = ({ children }: Props) => {
+const TextWrapper = ({ children, id }: Props) => {
     const text = React.useRef<HTMLDivElement>(null)
     const { scrollYProgress } = useScroll({
         target: text,
@@ -17,7 +18,7 @@ const TextWrapper = ({ children }: Props) => {
     const x = useTransform(scrollYProgress, [1, 0.4, 0], [0, 0, -1000])
 
     return (
-        <div ref={text}>
+        <div key={id} ref={text}>
             <motion.div style={{
                 opacity: opacity,
                 x: x,
@@ -32,7 +33,7 @@ export default function Skills() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {skills.map(skill => (
-                <TextWrapper>
+                <TextWrapper id={skill.id}>
                     <div key={skill.id} className="bg-gray-800 p-6 rounded-lg shadow-md md:h-52">
                         <div className="flex items-center mb-4">
                             <div className="w-10 h-10 bg-blue-500 flex items-center justify-center rounded-full">
