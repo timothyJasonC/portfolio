332304b6eb9a33f79bea53af1e1448e03669a8db
Timothy Jason <timothycaviezel@gmail.com>
2024-08-08 14:23:28 +0700

    fix mobile ui 3

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,10 +4,9 @@ import React from 'react'
 
 interface Props {
     children: React.ReactNode;
-    id: number
 }
 
-const TextWrapper = ({ children, id }: Props) => {
+const TextWrapper = ({ children }: Props) => {
     const text = React.useRef<HTMLDivElement>(null)
     const { scrollYProgress } = useScroll({
         target: text,
@@ -18,7 +17,7 @@ const TextWrapper = ({ children, id }: Props) => {
     const x = useTransform(scrollYProgress, [1, 0.4, 0], [0, 0, -1000])
 
     return (
-        <div key={id} ref={text}>
+        <div ref={text}>
             <motion.div style={{
                 opacity: opacity,
                 x: x,
@@ -33,19 +32,21 @@ export default function Skills() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {skills.map(skill => (
-                <TextWrapper id={skill.id}>
-                    <div key={skill.id} className="bg-gray-800 p-6 rounded-lg shadow-md md:h-52">
-                        <div className="flex items-center mb-4">
-                            <div className="w-10 h-10 bg-blue-500 flex items-center justify-center rounded-full">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v6a9 9 0 0018 0V7m-9 9a9 9 0 009-9m0 0a9 9 0 00-9-9 9 9 0 00-9 9" />
-                                </svg>
+                <div key={skill.id}>
+                    <TextWrapper>
+                        <div key={skill.id} className="bg-gray-800 p-6 rounded-lg shadow-md md:h-52">
+                            <div className="flex items-center mb-4">
+                                <div className="w-10 h-10 bg-blue-500 flex items-center justify-center rounded-full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v6a9 9 0 0018 0V7m-9 9a9 9 0 009-9m0 0a9 9 0 00-9-9 9 9 0 00-9 9" />
+                                    </svg>
+                                </div>
+                                <h2 className="text-xl font-semibold ml-4">{skill.title}</h2>
                             </div>
-                            <h2 className="text-xl font-semibold ml-4">{skill.title}</h2>
+                            <p>{skill.list}</p>
                         </div>
-                        <p>{skill.list}</p>
-                    </div>
-                </TextWrapper>
+                    </TextWrapper>
+                </div>
             ))}
         </div>
     )
